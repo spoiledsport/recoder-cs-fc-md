2bfedb2c870dd6f5febd3cedabdba90848c2153c
jansch@gmail.com <jansch@gmail.com@380f3ba6-a3c6-11de-bb10-370f7287a6b7>
2009-09-24 18:00:47 +0000

    person C# example for ReferenceTest.java

---
+++ trunk/recoder-cs-fc-md/test/personExp/Harry.cs
@@ -0,0 +1,39 @@
+using System;
+
+namespace Cars
+{
+
+public class Harry : Person
+{
+    public int age;
+	public Harry()
+    {
+        Console.WriteLine("Child Constructor.");
+    }
+
+	public String toString() {
+		return this.name + " : " + this.age;
+	}
+	
+	public String myAddedService() {
+		return "more Service";
+	}
+	
+	// nesting test
+	private int methodNesting(int k){
+		int i=k;
+		if(k==0){
+			k=1;
+		}else
+			for(int cnt=0; cnt<100;cnt++){
+		    	i= (int) (Math.random()*cnt+1);
+				if(i==13){
+	        		i=0;
+	        	}
+		}
+		return i;
+	  }
+	
+
+}
+}
