e948530854d7d5377c0a049b58ff1703c9b54f4a
jansch@gmail.com <jansch@gmail.com@380f3ba6-a3c6-11de-bb10-370f7287a6b7>
2009-10-06 20:58:48 +0000

    keymind parsing acceptance test

---
+++ trunk/recoder-cs-fc-md/test/src/fcMDtests/KeymindAcceptanceTest.java
@@ -0,0 +1,104 @@
+package fcMDtests;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+import recoder.ParserException;
+import recoder.io.DefaultSourceFileRepository;
+import recoder.list.StringArrayList;
+import recoder.list.StringMutableList;
+import recoder.util.FileCollector;
+import fcMDtests.util.FileSlurper;
+
+/**
+ * This unit test tests the acceptance rate for keymind projects. <br>
+ * The test is parametrized. In the method {@link #getTestFiles()} a
+ * {@link FileSlurper} to get an array of Files that match a certain regex. <br>
+ * The test is run with method {@link #test}.
+ * 
+ * @author Jan Schumacher, jansch@gmail.com
+ * @see #GenericsTest(String name)
+ */
+@RunWith(value = Parameterized.class)
+public class KeymindAcceptanceTest extends ParserTestCase {
+
+	// configuration options
+	static String testDir = "/Users/janschumacher/tmp/ARTSvn/trunk/ART/src";
+	static String regex = ".*cs_new.*$";;
+	String testFile;
+	static int testNumber = 0;
+
+	public KeymindAcceptanceTest(String testFile) {
+		this.testFile = testFile;
+	}
+
+	/**
+	 * This method parses the file that is currently beeing tested.
+	 * 
+	 * @param fileName
+	 *            the file to be prased
+	 */
+	private void parse(String fileName) throws IOException, ParserException {
+		cu = sc.getProgramFactory().parseCompilationUnit(
+				new FileReader(fileName));
+	}
+
+	/**
+	 * The method that supplies the files for the parametrized test case
+	 * {@link #test()}. It uses a {@link FileSlurper} to fill the field testFile
+	 * with the file to be tested.
+	 */
+	@Parameters
+	public static LinkedList<String[]> getTestFiles() {
+		
+		
+		FileCollector col = new FileCollector(testDir);
+		ArrayList<String> list = new ArrayList<String>();
+		LinkedList<String[]> params = new LinkedList<String[]>();
+
+		
+		while (col.next(DefaultSourceFileRepository.CSHARP_FILENAME_FILTER_NEW)) {
+			String path;
+			try {
+				path = col.getFile().getCanonicalPath();
+			} catch (IOException ioe) {
+				path = col.getFile().getAbsolutePath();
+			}
+			list.add(path);
+		}
+		
+		for (String f : list) {
+			params.add(new String[] { f });
+		}
+
+		return params;
+	}
+
+	/**
+	 * The paremetrized test case itself. It is data driven and gets the files
+	 * to parse from {@link #getTestFiles()}.
+	 * 
+	 * @throws ParserException
+	 */
+	@Test
+	public void test() throws IOException, ParserException {
+		System.out.println("PARSING file #" + testNumber++ + ":\n" + testFile);
+		try {
+			parse(testFile);
+		} catch (ParserException e) {
+			System.err.println("parser exception!");
+			throw e;
+		}
+
+	}
+
+}
